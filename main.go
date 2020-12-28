99c57c63d836d8e4bc3f4d4137775bd43172f598
Gia Duong Duc Minh <giaduongducminh@gmail.com>
2020-12-28 18:01:53 +0700

    #patch remove dangling images in 30 days

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
@@ -15,12 +17,24 @@ func main() {
 		panic(err)
 	}
 
-	images, err := cli.ImageList(ctx, types.ImageListOptions{All: true})
+	r, err := PruneUnusedImages(cli, ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, image := range images {
-		fmt.Println(image.ID)
+	fmt.Printf("Space Reclaimded: %d\n", r.SpaceReclaimed)
+
+	for _, dlt := range r.ImagesDeleted {
+		fmt.Printf("%s\n", dlt.Deleted)
 	}
+}
+
+// PruneUnsuedImages prunes images that dangling and built since 4w ago
+func PruneUnusedImages(cli *client.Client, ctx context.Context) (types.ImagesPruneReport, error) {
+	fargs := filters.NewArgs()
+	until := time.Now().AddDate(0, 0, -30).Unix()
+	fargs.Add("dangling", "true")
+	fargs.Add("until", fmt.Sprintf("%d", until))
+	r, err := cli.ImagesPrune(ctx, fargs)
+	return r, err
 }
